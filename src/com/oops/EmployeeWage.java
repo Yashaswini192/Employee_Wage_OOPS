3c6e9db7fb59b02dbac74f3e86e0a7cb217d500e
Yashaswini192 <voruganti.yashaswini@ust.com>
2023-02-09 23:34:58 +0530

    Using Random function to check Employee Attendance

--- src/com/oops/EmployeeWage.java
+++ src/com/oops/EmployeeWage.java
@@ -2,4 +2,16 @@ package com.oops;
 
 public class EmployeeWage {
 	
+	public static void main(String[] args) {
+		int isPresent = 1;
+		int isAbsent = 0;
+   	 
+		int checkAttendance = (int) ((Math.random() * 10) % 3);
+		if(checkAttendance == isPresent) {
+			System.out.println("employee is present");
+			}
+		else {
+			System.out.println("employee is absent");
+			}
+	}
 }
