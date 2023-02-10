26ef90a735a8021222d43058676f10c21dc0abef
Yashaswini192 <voruganti.yashaswini@ust.com>
2023-02-11 00:35:24 +0530

    Adding part time employee and Wages

--- src/com/oops/EmployeeWage.java
+++ src/com/oops/EmployeeWage.java
@@ -4,19 +4,24 @@ public class EmployeeWage {
 
 	static int isPresent = 1 ;
 	static int isAbsent = 0 ;
+	static int part_time = 2;
 	static int wagePerHour = 20;
-	static int fulldayHours = 8;
+	static int fulldayHours = 16;
 	
 	public void Attendance() {
 
-		int checkAttendance = (int) ((Math.random() * 10) % 2);
+		int checkAttendance = (int) ((Math.random() * 10) % 3);
 		if(checkAttendance == isPresent) {
 			System.out.println("employee is present");
-			fulldayHours = 8;
+			fulldayHours = 16;
 		}
 		else if (checkAttendance == isAbsent){
 			System.out.println("employee is absent");
 			fulldayHours = 0;
+		}
+		else if(checkAttendance == part_time) {
+			System.out.println("employee is working part_time");
+			fulldayHours = 8;
 		}
 		else {
 			System.out.println("employee is not working");
