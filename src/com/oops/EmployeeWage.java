83d4899c7b0a8de07b211cd87eafdc15908e834e
Yashaswini192 <voruganti.yashaswini@ust.com>
2023-02-11 00:27:06 +0530

    calculating daily employee wage

--- src/com/oops/EmployeeWage.java
+++ src/com/oops/EmployeeWage.java
@@ -1,17 +1,38 @@
 package com.oops;
 
 public class EmployeeWage {
+
+	static int isPresent = 1 ;
+	static int isAbsent = 0 ;
+	static int wagePerHour = 20;
+	static int fulldayHours = 8;
 	
-	public static void main(String[] args) {
-		int isPresent = 1;
-		int isAbsent = 0;
-   	 
-		int checkAttendance = (int) ((Math.random() * 10) % 3);
+	public void Attendance() {
+
+		int checkAttendance = (int) ((Math.random() * 10) % 2);
 		if(checkAttendance == isPresent) {
 			System.out.println("employee is present");
-			}
-		else {
+			fulldayHours = 8;
+		}
+		else if (checkAttendance == isAbsent){
 			System.out.println("employee is absent");
-			}
+			fulldayHours = 0;
+		}
+		else {
+			System.out.println("employee is not working");
+		}
+	}				
+}
+
+class DailyWages extends EmployeeWage{
+	
+	
+	public void dailywage() {
+		
+		int dailyWages = fulldayHours * wagePerHour ;
+		System.out.println("dailywages:"+dailyWages);
+
 	}
 }
+
+
---
+++ src/com/oops/Main.java
@@ -0,0 +1,11 @@
+package com.oops;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		EmployeeWage e = new EmployeeWage();
+		e.Attendance();
+		DailyWages d = new DailyWages();
+		d.dailywage();
+	}
+}
